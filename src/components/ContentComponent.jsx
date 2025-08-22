2d4a9f9aae529ca5409c1c1f53a5b26b8ff9a466
samirvithlani <samir.vithlani83955@gmail.com>
2025-08-22 11:57:37 +0530

    styling

--- src/components/ContentComponent.jsx
+++ src/components/ContentComponent.jsx
@@ -11,13 +11,13 @@ export const ContentComponent = () => {
   };
   
   return (
-    <div>
+    <div style={{textAlign:"center"}}>
       <h1>CONTENT COMPONENT....</h1>
       <h1>HELLO ALL !!!</h1>
       <p>EVERY TAG MUST HAVE CLOSING TAG</p>
       <p>At a time you can return only 1 tag</p>
       {name}
-      <h1>{name}</h1>
+      <h1 style={{color:"blue"}}>{name}</h1>
       <h3>Age = {age}</h3>
       <h3>Active = {isActive == true ? "Active" : "Not Active"}</h3>
       <h1>Name - {user.name}</h1>
--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
 export const FooterComponent = () => {
+  const footerStyle = {
+    height: "150px",
+    width: "auto",
+    backgroundColor: "black",
+    color: "white",
+    marginTop: "10px",
+    textAlign: "center",
+  };
+
   return (
-    <div>
-        <h1>FOOTER COMPONENT</h1>
+    // <div style={{height:"150px",width:"auto",backgroundColor:"black",color:"white",marginTop:"10px",textAlign:"center"}}>
+    //     <h1>FOOTER COMPONENT</h1>
+    // </div>
+    <div style={footerStyle}>
+      <h1>FOOTER COMPONENT</h1>
     </div>
-  )
-}
+  );
+};
